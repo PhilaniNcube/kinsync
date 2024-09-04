709abf9a1fbb4410ec89b9b4dcffab290f31cc70
Philani Ncube <ncbphi001@gmail.com>
2024-09-04 12:35:35 +0200

    chore: Update project details component to improve task completion UI

--- app/(dashboard)/dashboard/[org]/projects/[projectId]/_components/project-details.tsx
+++ app/(dashboard)/dashboard/[org]/projects/[projectId]/_components/project-details.tsx
@@ -49,97 +49,102 @@ export default function ProjectDetails({ projectInfo, tasks }: Props) {
 	const progress = (completedTasks / optimisticTasks.length) * 100;
 
 	return (
-		<div className="container p-6 mx-auto space-y-8">
-			<Card>
-				<CardHeader>
-					<div className="flex items-start justify-between">
-						<div>
-							<CardTitle className="text-3xl font-bold">
-								{projectInfo.name}
-							</CardTitle>
-							<CardDescription className="mt-2">
-								{projectInfo.description}
-							</CardDescription>
-						</div>
-						<Badge variant="outline" className="text-sm">
-							{progress.toFixed(0)}% Complete
-						</Badge>
-					</div>
-				</CardHeader>
-				<CardContent>
-					<div className="flex items-center mb-4 space-x-4 text-sm text-muted-foreground">
-						<div className="flex items-center">
-							<Calendar className="w-4 h-4 mr-2" />
-							Start: {format(projectInfo.start_date, "MMM d, yyyy")}
-						</div>
-						<div className="flex items-center">
-							<Calendar className="w-4 h-4 mr-2" />
-							End:{" "}
-							{projectInfo.end_date &&
-								format(projectInfo.end_date, "MMM d, yyyy")}
-						</div>
-					</div>
-					<Progress value={progress} className="mb-4" />
-				</CardContent>
-			</Card>
+    <div className="container p-6 mx-auto space-y-8">
+      <Card>
+        <CardHeader>
+          <div className="flex items-start justify-between">
+            <div>
+              <CardTitle className="text-3xl font-bold">
+                {projectInfo.name}
+              </CardTitle>
+              <CardDescription className="mt-2">
+                {projectInfo.description}
+              </CardDescription>
+            </div>
+            <Badge variant="outline" className="text-sm hidden md:block">
+              {progress.toFixed(0)}% Complete
+            </Badge>
+          </div>
+        </CardHeader>
+        <CardContent>
+          <div className="flex items-center justify-between mb-4 space-x-4 text-sm text-muted-foreground">
+            <div className="flex items-center text-xs">
+              <Calendar className="w-4 h-4 mr-2 hidden md:block" />
+              Start: {format(projectInfo.start_date, "MMM d, yyyy")}
+            </div>
+            <div className="flex items-center text-xs">
+              <Calendar className="w-4 h-4 mr-2 hidden md:block" />
+              End:{" "}
+              {projectInfo.end_date &&
+                format(projectInfo.end_date, "MMM d, yyyy")}
+            </div>
+          </div>
+          <Progress value={progress} className="mb-4" />
+        </CardContent>
+      </Card>
 
-			<Card>
-				<CardHeader className="flex flex-row items-center justify-between">
-					<CardTitle>Project Tasks</CardTitle>
-					<CreateTaskDialog
-						tenant_id={projectInfo.tenant_id}
-						project_id={projectInfo.id}
-					/>
-				</CardHeader>
-				<CardContent>
-					<ul className="space-y-4">
-						{optimisticTasks.map((task) => (
-							<li
-								key={task.id}
-								className="flex items-center justify-between p-2 rounded-lg hover:bg-muted"
-							>
-								<div className="flex items-center space-x-2">
-									<Checkbox
-										checked={task.completed}
-										onCheckedChange={(checked) => {
-                      console.log(checked)
+      <Card>
+        <CardHeader className="flex flex-col md:flex-row md:items-center justify-between">
+          <CardTitle>Project Tasks</CardTitle>
+          <CreateTaskDialog
+            tenant_id={projectInfo.tenant_id}
+            project_id={projectInfo.id}
+          />
+        </CardHeader>
+        <CardContent>
+          <ul className="space-y-4">
+            {optimisticTasks.map((task) => (
+              <li
+                key={task.id}
+                className="flex items-center justify-between p-2 rounded-lg hover:bg-muted"
+              >
+                <div className="flex items-center space-x-2">
+                  <Checkbox
+                    checked={task.completed}
+                    onCheckedChange={(checked) => {
+                      console.log(checked);
                       setOptimisticTasks({
                         ...task,
                         completed: checked === true,
-                      })
+                      });
 
-                      completeTaskAction(checked === true, task.id, projectInfo.tenant_id, projectInfo.id)
+                      completeTaskAction(
+                        checked === true,
+                        task.id,
+                        projectInfo.tenant_id,
+                        projectInfo.id
+                      );
                     }}
-									/>
-									<span
-										className={
-											task.completed ? "line-through text-muted-foreground" : ""
-										}
-									>
-										{task.description}
-									</span>
-								</div>
-								<Badge variant="secondary">
-									{task.cost && formatCurrency(task.cost)}
-								</Badge>
-							</li>
-						))}
-					</ul>
-					<div className="mt-4 font-semibold text-right">
-						Total Cost:{" "}
-						{formatCurrency(tasks.reduce((sum, task) => sum + task.cost, 0))}
-					</div>
-				</CardContent>
-			</Card>
+                  />
+                  <span
+                    className={
+                      task.completed ? "line-through text-muted-foreground" : ""
+                    }
+                  >
+                    {task.description}
+                  </span>
+                </div>
+                <Badge variant="secondary">
+                  {task.cost && formatCurrency(task.cost)}
+                </Badge>
+              </li>
+            ))}
+          </ul>
+          <div className="mt-4 font-semibold text-right">
+            Total Cost:{" "}
+            {formatCurrency(tasks.reduce((sum, task) => sum + task.cost, 0))}
+          </div>
+        </CardContent>
+      </Card>
 
-			<div className="grid gap-6 md:grid-cols-2">
-				<Card>
-					<CardHeader>
-						<CardTitle>Project Documents</CardTitle>
-					</CardHeader>
-					<CardContent>
-						<ul className="space-y-2">
-							{/* {project.documents.map((doc) => (
+      <div className="grid gap-6 md:grid-cols-2">
+        <Card>
+          <CardHeader>
+            <CardTitle>Project Documents</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <ul className="space-y-2">
+              {/* {project.documents.map((doc) => (
 								<li key={doc.id} className="flex items-center space-x-2">
 									<FileText className="w-4 h-4" />
 									<span>{doc.name}</span>
@@ -148,17 +153,17 @@ export default function ProjectDetails({ projectInfo, tasks }: Props) {
 									</Button>
 								</li>
 							))} */}
-						</ul>
-					</CardContent>
-				</Card>
+            </ul>
+          </CardContent>
+        </Card>
 
-				<Card>
-					<CardHeader>
-						<CardTitle>Project Images</CardTitle>
-					</CardHeader>
-					<CardContent>
-						<div className="grid grid-cols-2 gap-4">
-							{/* {project.images.map((image) => (
+        <Card>
+          <CardHeader>
+            <CardTitle>Project Images</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <div className="grid grid-cols-2 gap-4">
+              {/* {project.images.map((image) => (
 								<div key={image.id} className="space-y-2">
 									<Image
 										src={image.url}
@@ -170,10 +175,10 @@ export default function ProjectDetails({ projectInfo, tasks }: Props) {
 									<p className="text-sm text-center">{image.name}</p>
 								</div>
 							))} */}
-						</div>
-					</CardContent>
-				</Card>
-			</div>
-		</div>
-	);
+            </div>
+          </CardContent>
+        </Card>
+      </div>
+    </div>
+  );
 }
